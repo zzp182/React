61f5f4e819a89e0d347d2c96c35885904b911823
zzp812875759 <zhangzhengpeng2021@163.com>
2021-12-02 11:00:22 +0800

    初始化路由组件

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route , Router , HashRouter , Link } from 'react-router-dom';
+import React from 'react';
+// 引用组件
+import Home from './views/Home'
+import About from './views/About'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} 
+  class App extends React.Component{
+    constructor(props) {
+      super(props);
+      this.state = {}
+    }
+    render() {
+      return (
+        <BrowserRouter>
+          <Switch>
+            <Route exact component={Home}  path="/" />
+            <Route exact component={About}  path="/about" />
+          </Switch>
+        </BrowserRouter> 
+      )
+    }
+  }
+
 
 export default App;
---
+++ react-admin/src/views/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+  class About extends React.Component{
+    constructor(props) {
+      super(props);
+      this.state = {}
+    }
+    render() {
+      return <div>About</div>
+    }
+  }
+  export default About;
